22cd3317d687c1c09b06884a911171107cbff1aa
Charlie Poole <charliepoole@gmail.com>
2016-12-29 00:17:14 -0800

    Tests should check result state of theories that have no data

--- src/NUnitFramework/tests/Attributes/TheoryTests.cs
+++ src/NUnitFramework/tests/Attributes/TheoryTests.cs
@@ -42,15 +42,15 @@ namespace NUnit.Framework.Attributes
         }
 
         [Test]
-        public void TheoryWithNoDatapointsIsRunnable()
+        public void TheoryWithNoDatapointsIsRunnableButFails()
         {
-            TestAssert.IsRunnable(fixtureType, "TheoryWithArgumentsButNoDatapoints");
+            TestAssert.IsRunnable(fixtureType, "TheoryWithArgumentsButNoDatapoints", ResultState.Failure);
         }
 
         [Test]
-        public void UnsupportedNullableTypeArgumentWithNoDatapointsAreRunnable()
+        public void UnsupportedNullableTypeArgumentWithNoDatapointsIsRunnableButFail()
         {
-            TestAssert.IsRunnable(fixtureType, "TestWithUnsupportedNullableTypeArgumentWithNoDataPoints");
+            TestAssert.IsRunnable(fixtureType, "TestWithUnsupportedNullableTypeArgumentWithNoDataPoints", ResultState.Failure);
         }
 
         [Test]
